f40951366550109aa6fe0513b9109053d081158d
jongwon <wengers1004@naver.com>
2020-09-14 00:14:25 +0900

    refactor: middleware 필요한 key값만 사용
    middleware에서 사용되는 key 값만 반환되도록 변경.

--- CommL/base.js
+++ CommL/base.js
@@ -1,49 +1,41 @@
 const middleware = (data) => {
-    data.id = data.id != undefined ? ` id=${data.id}`: "";
-    data.Class = data.Class != undefined ? ` class=${data.Class}`: "";
-    data.action = data.action != undefined ? ` action=${data.action}`: "";
-    data.method = data.method != undefined ? ` method=${data.method}`: "";
-    data.children = data.children != undefined ? data.children : "";
-    data.name = data.name != undefined ? `name=${data.name}` : "";
-    data.type = data.type != undefined ? `type=${data.type}` : "";
-    data.placeholder = data.placeholder != undefined ? `placeholder=${data.placeholder}` : "";
-    data.src = data.src != undefined ? `src=${data.src}` : "";
-    data.width = data.width != undefined ? `width=${data.width}` : "";
-    data.height = data.height != undefined ? `height=${data.height}` : "";
-    return data;
+    delete data.children
+    delete data.component
+    return Object.keys(data).map(key => ` ${key} = ${data[key]} `)
 }
+// children, component는 속성이 아니기 때문에 키 값을 제거해줍니다.
+// 만약 children, component가 필요한 경우는 호출하기 전에 따로 저장해주세요.
+// 아래 예시 참고..
 
 const input = (data) => {
-    const { type, name, placeholder, Class, id} = middleware(data);
-    return `<input ${type} ${name} ${placeholder} ${Class} ${id}>`;
+    return `<input ${middleware(data)}>`;
 }
 
 const button = (data) => {
-    const{ name, id, Class, value } = middleware(data);
-    data.type = data.type === 'type=submit' ? ` ${data.type}` : ` type="button"` 
-    return `<button ${name} ${data.type} ${id} ${Class}> ${value} </button>`;
+    const { value } = data
+    return `<button ${middleware(data)}> ${value} </button>`;
 }
 
 const img = (data) => {
-    const { src, width, height, id, Class } = middleware(data);
-    return `<img ${src} ${width} ${height} ${id} ${Class}>`;
+    return `<img ${middleware(data)}>`;
 }
 
 const select = (data) => {
-    let { children, selected, name, id, Class } = middleware(data);
+    let { children } = data
+    const attribute = middleware(data)
     children = children.map(element => `<option ${element}>${element}</option>`)
                                  .reduce((acc,val) => acc += val, '');
     children = `<option ${selected} selected="selected">${selected}</option>` + children;
-    return `<select ${name} ${id} ${Class}> ${children} </select>`;
+    return `<select ${attribute}> ${children} </select>`;
 }
 
 const form = (data) => {
-    let { action, method, id, Class, children} = middleware(data);
-    let contents = `<form ${action} ${method} ${id} ${Class}> ${children} </form>`;
-
+    let {children=''} = data
+    const attribute = middleware(data)
+    let contents = `<form ${attribute}> ${children} </form>`;
     const setContents = (...child) => {
         children += child.reduce((acc,val) => acc += val,'');
-        contents = `<form ${action} ${method} ${id} ${Class}> ${children} </form>`;
+        contents = `<form ${attribute}> ${children} </form>`;
     }
 
     const getContents = () => contents;
@@ -52,13 +44,14 @@ const form = (data) => {
 }
 
 const header = (data) => {
-    let { id, Class, children} = middleware(data);
-    console.log('header', Class, id, children);
-    let contents = `<header ${id} ${Class}> ${children} </header>`;
+    let {children=''} = data
+    const attribute = middleware(data)
+    console.log('header', attribute);
+    let contents = `<header ${attribute}> ${children} </header>`;
 
     const setContents = (...child) => {
         children += child.reduce((acc,val) => acc += val,'');
-        contents = `<header ${id} ${Class}> ${children} </header>`;
+        contents = `<header ${attribute}> ${children} </header>`;
     }
 
     const getContents = () => contents;
@@ -67,12 +60,13 @@ const header = (data) => {
 }
 
 const div = (data) => {
-    let{ id, Class, children } = middleware(data);
-    let contents = `<div${id}${Class}>${children}</div>`;
+    let {children=''} = data
+    const attribute = middleware(data)
+    let contents = `<div${attribute}>${children}</div>`;
 
     const setContents = (...child) => {
         children += child.reduce((acc,val) => acc += val,'');
-        contents = `<div${id}${Class}>${children}</div>`;
+        contents = `<div${attribute}>${children}</div>`;
     }
 
     const getContents = () => contents;
@@ -81,12 +75,13 @@ const div = (data) => {
 }
 
 const component = (data) => {
-    let{ component, id, Class, children } = middleware(data);
-    let contents = `<${component}${id}${Class}>${children}</${component}>`;
+    let {component, children=''} = data
+    const attribute = middleware(data)
+    let contents = `<${component}${attribute}>${children}</${component}>`;
 
     const setContents = (...child) => {
         children += child.reduce((acc,val) => acc += val,'');
-        contents = `<${component}${id}${Class}>${children}</${component}>`;
+        contents = `<${component}${attribute}>${children}</${component}>`;
     }
 
     const getContents = () => contents;
